31fd53da8618ff17265bbbfc8a76d8d9b5adf5d6
ALUSHIJIE <131883112+ALUSHIJIE@users.noreply.github.com>
2023-12-10 03:02:20 +0900

    Add files via upload

---
+++ Assets/Script/FollowCar.cs
@@ -0,0 +1,17 @@
+﻿using UnityEngine;
+
+public class FollowCar : MonoBehaviour
+{
+    public Transform target; // 将模型车的Transform组件拖放到这里
+    public Vector3 offset = new Vector3(0f, 5f, -10f); // 相机相对于模型车的偏移
+
+    private void Update()
+    {
+        if (target != null)
+        {
+            // 让相机跟随模型车移动
+            transform.position = target.position + offset;
+            transform.LookAt(target);
+        }
+    }
+}
---
+++ Assets/Script/ShootControl.cs
@@ -0,0 +1,47 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ShootController : MonoBehaviour
+{
+    public GameObject redBallPrefab; // 引用红色小球预制体
+    private GameObject currentRedBall; // 用于存储当前的红色小球
+    public float shootSpeed = 40f; // 小球的发射速度
+
+    void Update()
+    {
+        // 检测鼠标左键点击
+        if (Input.GetMouseButtonDown(0))
+        {
+            // 销毁当前的红色小球（如果存在）
+            Destroy(currentRedBall);
+            // 创建新的红色小球并发射
+            currentRedBall = InstantiateRedBall();
+
+        }
+
+        // 更新红色小球的位置
+        if (currentRedBall != null)
+        {
+            Vector3 newPosition = currentRedBall.transform.position + currentRedBall.transform.forward * shootSpeed * Time.deltaTime;
+            currentRedBall.transform.position = newPosition;
+        }
+    }
+
+    GameObject InstantiateRedBall()
+    {
+        // 实例化红色小球，并设置其位置为模型位置
+        GameObject redBall = Instantiate(redBallPrefab, transform.position, transform.rotation);
+        // 将小球的颜色设置为红色
+        redBall.GetComponent<Renderer>().material.color = Color.red;
+
+        // 获取小球的刚体组件
+        Rigidbody rb = redBall.GetComponent<Rigidbody>();
+        if (rb != null) // 移除刚体组件，不再受物理引擎影响
+        {
+            Destroy(rb);
+        }
+
+        return redBall;
+    }
+}
